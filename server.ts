939871851f82d11f87546d457694ddef5ac25734
Ivan <78872275+Hikk0o@users.noreply.github.com>
2022-04-26 23:25:04 +0300

    Initial commit

---
+++ server.ts
@@ -0,0 +1,75 @@
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+    key: fs.readFileSync('ssl/apache2.key'),
+    cert: fs.readFileSync('ssl/apache2.cert')
+};
+
+
+https.createServer(options, function (req, res) {
+    console.log(req.url)
+    const sitePath = './site/';
+    switch (req.url) {
+        case '/favicon.ico':
+            res.end()
+            break
+        case '/' || '/index.html':
+            fs.readFile(sitePath + 'index.html', null, function (error, html) {
+                if (error) {
+                    res.writeHead(404);
+                    res.write('Whoops! File not found!');
+                } else {
+                    res.writeHead(301, {
+                        'Content-Type': 'text/html',
+                    });
+                    res.write(html);
+                }
+                res.end();
+            });
+            break
+        default:
+            let redirects = JSON.parse(fs.readFileSync('./redirects.json', 'utf8'));
+            if (redirects[req.url] !== undefined) {
+                res.writeHead(301,{
+                    Location: redirects[req.url]
+                });
+                res.end()
+            } else if (req.url.endsWith('.js') || req.url.endsWith('.css')) {
+                let contentType = ''
+                if (req.url.endsWith('.js')) {
+                    contentType = 'text/javascript'
+                }
+                if (req.url.endsWith('.css')) {
+                    contentType = 'text/css'
+                }
+                fs.readFile(sitePath + req.url, null, function (error, html) {
+                    if (error) {
+                        res.writeHead(404);
+                        res.write('Whoops! File not found!');
+                    } else {
+                        res.writeHead(200, {
+                            'Content-Type': contentType,
+                            'Cache-Control': 'max-age=31536000'
+                        });
+                        res.write(html);
+                    }
+                    res.end();
+                });
+            } else {
+                fs.readFile(sitePath + 'errors/404.html', null, function (error, html) {
+                    if (error) {
+                        res.writeHead(404);
+                        res.write('Whoops! File not found!');
+                    } else {
+                        res.writeHead(200, {
+                            'Content-Type': 'text/html',
+                            'Cache-Control': 'max-age=31536000'
+                        });
+                        res.write(html);
+                    }
+                    res.end();
+                });
+            }
+    }
+}).listen(443);
---
+++ site/js/index-v2.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.element', {
+    strings: [
+        '<a class="special-color special-font">junior software engineer</a>.',
+        '^300 <a class="special-color special-font">student of Russian Technological University</a>.'
+    ],
+    typeSpeed: 30,
+    loop: true,
+    contentType: 'html',
+    backDelay: 3500,
+    startDelay: 300,
+    backSpeed: 10
+});
